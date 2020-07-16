0fd6d84785b283298670ea17d1cd4d350597a16a
Serhii Malyshko <malyshkoserhii91@gmail.com>
2020-07-16 14:33:40 +0300

    task 3 some positive results

--- js/task-03.js
+++ js/task-03.js
@@ -1,21 +1,32 @@
 "use strict";
 
-let word;
-let wordLength = 1;
+// Кожний рядок розбити в масив
+// В кожного елементу масивуб тобто в слова можна подивитися довжину
+// Кожний елемент масиву = слово
 
 const findLongestWord = function (string) {
   const words = string.split(" ");
-
-  for (word of words) {
-    const i = word.length;
-
-    if (wordLength < i) {
-      wordLength = i;
-      break;
+  const longestWord = words[0].length;
+  console.log('longestWorld', longestWord);
+  console.log('Масив', words);
+  for (let i = 0; i < words.length; i += 1) {
+    let length = words[i].length;
+    for(let j = 0; j < length; j += 1) {
+      
+      console.log(j);
     }
 
-    console.log(i);
+
+    console.log('Слово - елемент масиву', words[i]);
+    console.log('Довжина слова', length);
+    if (length > longestWord) {
+      length = longestWord;
+    }
   }
+
+  // const length = words.length;
+  // console.log(length);
+  return longestWord;
 };
 
 console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
