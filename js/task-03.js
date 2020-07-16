2fa1b4f3a16ce750360e04631b152116917527af
Serhii Malyshko <malyshkoserhii91@gmail.com>
2020-07-16 16:09:10 +0300

    task 3 length of word is find

--- js/task-03.js
+++ js/task-03.js
@@ -4,30 +4,28 @@
 // В кожного елементу масивуб тобто в слова можна подивитися довжину
 // Кожний елемент масиву = слово
 
+// Як показати найдовше слово якщо ми знаємо його довжину?
+
 const findLongestWord = function (string) {
   const words = string.split(" ");
-  let longestWord = words[0].length;
-  console.log('Припускаємо, що перше слово масиву найдовшеб тобто longestWorld', longestWord);
-  console.log('Масив', words);
+  let wordLength = words[0].length;
+  console.log(
+    "Припускаємо, що перше слово масиву найдовше, тобто longestWorld",
+    wordLength
+  );
+  console.log("Масив", words);
   for (let i = 0; i < words.length; i += 1) {
     let length = words[i].length;
-    if(length > longestWord) {
-      longestWord = length;
+    if (length > wordLength) {
+      wordLength = length;
     }
-
-    // for(let j = 0; j < length; j += 1) {
-    //   if(j > longestWord) {
-    //     j = longestWord;
-    //     break;
-    //   }
-    //   console.log('j', j);
-    // }
-
-    console.log('words[i] Слово - елемент масиву', words[i]);
-    console.log('lenght - Довжина слова', length);
+    console.log("words[i] - Слово-елемент масиву [words]", words[i]);
+    console.log("lenght - Довжина цього слова", length);
   }
 
-  return longestWord;
+  console.log(`Найдовше слово має   ${wordLength}   символів`);
+
+  return wordLength;
 };
 
 console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
