a9c12003e7c866bbd3aec611d0e1a50d95fb333d
Serhii Malyshko <malyshkoserhii91@gmail.com>
2020-07-22 16:43:54 +0300

    task 6 is added

--- js/task-06.js
+++ js/task-06.js
@@ -1,25 +1,21 @@
 "use strict";
 
-const sum = function (figures) {
-  let input;
-  let total = 0;
-  do {
-    const numbers = [];
+let number;
+let total = 0;
+const numbers = [];
 
-    input = prompt("Було введено не число, будь ласка, спробуйте ще раз");
-    numbers.push(input);
+do {
+  number = prompt("Введіть число");
+  if (isNaN(Number(number))) {
+    alert("Було введено не число, будь ласка, спробуйте ще раз");
+    continue;
+  } else {
+    numbers.push(number);
+  }
+} while (number !== null);
 
-    if (isNaN(Number(input))) {
-      alert("Ви не ввели число");
-      continue;
-    } else {
-      for (let i = 0; i < numbers.length; i += 1) {
-        total += +numbers;
-      }
-    }
-  } while (input !== null);
+for (number of numbers) {
+  total += +number;
+}
 
-  return `Загальна сума: ${total}`;
-};
-
-console.log(sum());
+console.log(`Загальна сума: ${total}`);
